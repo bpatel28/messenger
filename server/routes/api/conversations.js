3c5eba91f86894ee938cda5553cc3dbce7b01971
Brijesh Patel <b.patel9528@gmail.com>
2021-08-12 23:47:33 -0600

    fixed bug for conversation sorting and message sorting

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,9 +19,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "ASC"]],
+      order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "ASC"] },
+        { model: Message },
         {
           model: User,
           as: "user1",
@@ -68,7 +68,8 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
+      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.messages.reverse();
       conversations[i] = convoJSON;
     }
 
