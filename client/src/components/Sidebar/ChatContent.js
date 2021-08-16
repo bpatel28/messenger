ba365fb0918e73a05b87750edd4fa38c7fce7c1d
Brijesh Patel <b.patel9528@gmail.com>
2021-08-15 20:02:22 -0600

    optimize notification count to change UI after render

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography, Chip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -18,16 +18,23 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
-  previewDarkText : {
+  previewDarkText: {
     fontWeight: "bold",
     fontSize: 12,
     color: "#000000",
     letterSpacing: -0.17,
   },
-  chip: {
+  visibleChip: {
     maarginTop: theme.spacing(1),
     marginRight: theme.spacing(1),
-    padding: theme.spacing(0)
+    padding: theme.spacing(0),
+    display: "inline",
+  },
+  hiddenChip: {
+    maarginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    padding: theme.spacing(0),
+    display: "none",
   },
 }));
 
@@ -36,7 +43,35 @@ const ChatContent = (props) => {
 
   const { conversation } = props;
   const { latestMessageText, otherUser } = conversation;
-  const unreadCount = countUnreadMessages(conversation) || "";
+
+  const [unreadCount, setCount] = useState("");
+  const [chipStyle, setChipStyle] = useState(classes.hiddenChip);
+  useEffect(() => {
+    const count = countUnreadMessages(conversation);
+    if (unreadCount !== count) {
+      setCount(count);
+    }
+    if (unreadCount !== 0) {
+      setChipStyle(classes.visibleChip);
+    } else {
+      setChipStyle(classes.hiddenChip);
+    }
+  }, [
+    conversation,
+    unreadCount,
+    chipStyle,
+    classes.visibleChip,
+    classes.hiddenChip,
+  ]);
+
+  const [latestMsgStyle, setLatestMsgStyle] = useState(classes.previewText);
+  useEffect(() => {
+    if (unreadCount !== 0) {
+      setLatestMsgStyle(classes.previewDarkText);
+    } else {
+      setLatestMsgStyle(classes.previewText);
+    }
+  }, [unreadCount, classes.previewDarkText, classes.previewText]);
 
   return (
     <Box className={classes.root}>
@@ -44,21 +79,20 @@ const ChatContent = (props) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={unreadCount ? classes.previewDarkText : classes.previewText}>
-          {latestMessageText}
-        </Typography>
+        <Typography className={latestMsgStyle}>{latestMessageText}</Typography>
       </Box>
-      {unreadCount ? (
-        <Chip size="small" className={classes.chip} label={unreadCount} color="primary" />
-      ) : (
-        ""
-      )}
+      <Chip
+        size="small"
+        className={chipStyle}
+        label={unreadCount}
+        color="primary"
+      />
     </Box>
   );
 };
 
 const countUnreadMessages = (conversation) => {
-  const { messages, myLastRead = '1', otherUser } = conversation;
+  const { messages, myLastRead = "1", otherUser } = conversation;
   const unreadMessages = messages.filter(
     (message) =>
       Date.parse(message.updatedAt) > Date.parse(myLastRead) &&
