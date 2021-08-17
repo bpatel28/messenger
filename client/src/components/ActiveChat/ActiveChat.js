8b4ef2e926a24800ec35a503c84bad24785aa13c
Brijesh Patel <b.patel9528@gmail.com>
2021-08-17 02:52:07 -0600

    connected UI changes with redux.

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -3,12 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { Input, Header, Messages } from "./index";
 import { connect } from "react-redux";
+import { sendReadReceipt } from "../../store/utils/thunkCreators";
 
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexGrow: 8,
-    flexDirection: "column"
+    flexDirection: "column",
   },
   chatContainer: {
     marginLeft: 41,
@@ -16,14 +17,24 @@ const useStyles = makeStyles(() => ({
     display: "flex",
     flexDirection: "column",
     flexGrow: 1,
-    justifyContent: "space-between"
-  }
+    justifyContent: "space-between",
+  },
 }));
 
 const ActiveChat = (props) => {
   const classes = useStyles();
   const { user } = props;
   const conversation = props.conversation || {};
+
+  const handleMessageRead = async () => {
+    if (conversation?.id && conversation?.myUnreadMessageCount) {
+      const body = {
+        conversationId: conversation.id,
+        senderId: conversation.otherUser.id,
+      };
+      await props.sendReadReceipt(body);
+    }
+  };
 
   return (
     <Box className={classes.root}>
@@ -33,7 +44,7 @@ const ActiveChat = (props) => {
             username={conversation.otherUser.username}
             online={conversation.otherUser.online || false}
           />
-          <Box className={classes.chatContainer}>
+          <Box className={classes.chatContainer} onClick={handleMessageRead}>
             <Messages
               messages={conversation.messages}
               otherUser={conversation.otherUser}
@@ -43,6 +54,7 @@ const ActiveChat = (props) => {
               otherUser={conversation.otherUser}
               conversationId={conversation.id}
               user={user}
+              myUnreadMessageCount={conversation.myUnreadMessageCount}
             />
           </Box>
         </>
@@ -57,9 +69,18 @@ const mapStateToProps = (state) => {
     conversation:
       state.conversations &&
       state.conversations.find(
-        (conversation) => conversation.otherUser.username === state.activeConversation
-      )
+        (conversation) =>
+          conversation.otherUser.username === state.activeConversation
+      ),
   };
 };
 
-export default connect(mapStateToProps, null)(ActiveChat);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    sendReadReceipt: (id) => {
+      dispatch(sendReadReceipt(id));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActiveChat);
--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Chip, Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,14 +18,43 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  previewDarkText: {
+    fontWeight: "bold",
+    fontSize: 12,
+    color: "#000000",
+    letterSpacing: -0.17,
+  },
+  vibleChip: {
+    maarginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    padding: theme.spacing(0),
+    display: "flex",
+  },
+  hiddenChip: {
+    maarginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    padding: theme.spacing(0),
+    display: "none",
+  },
 }));
 
 const ChatContent = (props) => {
   const classes = useStyles();
 
-  const { conversation } = props;
-  const { latestMessageText, otherUser } = conversation;
+  const notificationMessage = (msgCount) => {
+    if (msgCount > 99) {
+      return "99+";
+    } else if (msgCount > 0) {
+      return msgCount;
+    } else {
+      return "";
+    }
+  };
 
+  const { conversation } = props;
+  const { latestMessageText, otherUser, myUnreadMessageCount } = conversation;
+  const message = notificationMessage(myUnreadMessageCount);
+  const chipStyle = message ? classes.vibleChip : classes.hiddenChip;
   return (
     <Box className={classes.root}>
       <Box>
@@ -36,6 +65,12 @@ const ChatContent = (props) => {
           {latestMessageText}
         </Typography>
       </Box>
+      <Chip
+        size="small"
+        className={chipStyle}
+        label={message}
+        color="primary"
+      />
     </Box>
   );
 };
